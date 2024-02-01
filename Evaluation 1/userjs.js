08844505220e5f8ce1d03a41039c6f7b462a0fea
Vraj Patel <patelvraj16042002@gmail.com>
2024-02-01 17:16:52 +0530

    Final

--- Evaluation 1/userjs.js
+++ Evaluation 1/userjs.js
@@ -1,18 +1,22 @@
 function addData(){
+    let index=Date.now();
     let userName=document.getElementById("txtUserName").value;
     let userEmail=document.getElementById("userEmail").value;
     let userPassword=document.getElementById("userPassword").value;
     let userDob=document.getElementById("userDob").value;
 
     let data={
+        id:index,
         name: userName,
         email: userEmail,
         password: userPassword,
         Dob: userDob
     };
-    
+
     let datas= sessionStorage.getItem("datas");
     datas= datas ? JSON.parse(datas) : [];
     datas.push(data);
-    sessionStorage.setItem("datas",JSON.stringify(datas))
+    sessionStorage.setItem("datas",JSON.stringify(datas));
+
+    
 }
