b824d84257abb664143cfa01173d6d1543833391
Vraj Patel <patelvraj16042002@gmail.com>
2024-02-28 17:02:55 +0530

    searching, sorting and filter added

---
+++ API/Product list/Update/scriptUpdate.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get('id');
+    console.log(productId)
+
+    fetch('https://api.escuelajs.co/api/v1/products/' + productId)
+    .then(response => {
+        // console.log(response)
+        if(response.ok) {
+            console.log('Product fetched successfully...!!')
+            return response.json();
+        } else {
+            console.log('Failed to fetch..!!', response.status);
+        }
+    })
+    .then(product => {
+        console.log(product);
+        document.getElementById('txtproductTitle').value = product.title;
+        document.getElementById('numProductPrice').value = product.price;
+        document.getElementById('txtareaDescription').value = product.description;
+        document.getElementById('imgLink').value = product.images.join(",");
+    })
+    .catch(error => console.log(error))
+
+    document.getElementById('updateForm').addEventListener('submit', (event) => {
+
+        console.log(event);
+        event.preventDefault();
+
+        if (validateForm()) {
+            console.log("hello")
+            const updateProduct = {
+                title: document.getElementById('txtproductTitle').value,
+                price: document.getElementById('numProductPrice').value,
+                description: document.getElementById('txtareaDescription').value,
+                images: document.getElementById('imgLink').value
+                    .split(",")
+                    .map(function (url) {
+                      return url.trim();
+                    }),
+            };
+   
+            fetch('https://api.escuelajs.co/api/v1/products/' + productId, {
+                method: "PUT",
+                headers: {
+                    "Content-type": 'application/json'
+                },
+                body: JSON.stringify(updateProduct)
+            })
+            .then(response => {
+                console.log(response)
+                if(response.ok) {
+                    alert('Product updated successfully..!!');
+                    return response.json();
+                } else {
+                    console.log('Failed to update..!!')
+                }
+            })
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+        }
+    });
+
+    function validateForm() {
+        const title = document.getElementById('txtproductTitle').value.trim();
+        const price = document.getElementById('numProductPrice').value.trim();
+        const description = document.getElementById('txtareaDescription').value.trim();
+        const imgLink = document.getElementById('imgLink').value.trim();
+
+        if (title === '' || price === '' || description === '' || imgLink === '') {
+            return false;
+        }
+
+        return true;
+    }
+});
--- API/Product list/script.js
+++ API/Product list/script.js
@@ -1,101 +1,187 @@
+  let data = [];
+  let currentPage = 1;
+  const itemsPerPage = 12;
+  filteredProducts=[];
 
-let data = [];
-
-function fetchProduct() {
-    return new Promise((resolve, reject) => {
-    fetch("https://api.escuelajs.co/api/v1/products")
-   .then((res) => res.json())
-   .then((products) => {
-     console.log(products);
-     resolve(products);
-   })
-   .catch((err) => {
-     console.log(err);
-   });
-});
-}
-fetchProduct()
- .then((products) => {
-   console.log(products);
-   data=products;
-   displayProducts(products);
- })
- .catch((err) => {
-   console.error(err);
- });
-// function fetchProduct() {
-//   fetch("https://api.escuelajs.co/api/v1/products")
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//       }
-//       return response.json();
-//     })
-//     .then(products => {
-//       data = products; // Store fetched data
-//       displayProducts(products); // Render products initially
-//     })
-//     .catch(error => {
-//       console.log('Error fetching data:', error);
-//     });
-// }
-
-function displayProducts(product){
- const container = document.getElementById("product-container");
- container.innerHTML = "";
- product.forEach((product) => {
- const createDiv = document.createElement("div");
- createDiv.classList.add("card");
- createDiv.innerHTML = `
-   <div>${product.title}</div>
-   <div class="image-container">
-      <img src="${product.images[0]}" alt="image" width="50" />
-   </div>
-   <div>Rs.${product.price}</div>
-   <details><summary>Description</summary>
-   ${product.description}<br>
-
-   </details>
-   <button onClick="deleteProduct(${product.id})">Delete</button>
-   `;
- container.appendChild(createDiv);
-});
-}
-
-
-const sortSelect = document.getElementById('sort-select');
-sortSelect.addEventListener('change', () => {
-  const sortBy = sortSelect.value;
-  if (sortBy === 'price-low-to-high') {
-      data.sort((a, b) => a.price - b.price);
-  } else if (sortBy === 'price-high-to-low') {
-      data.sort((a, b) => b.price - a.price);
+fetch("https://api.escuelajs.co/api/v1/categories")
+.then((response) => {
+  if (!response.ok) {
+    throw new Error("Failed to fetch categories.");
   }
-  displayProducts(data);
-});
-
-function deleteProduct(productId){
-  const userResponse=confirm('Do you want to delete');
-  if(userResponse){
-   fetch(`https://api.escuelajs.co/api/v1/products/${productId}`,{
-    method:"DELETE"
-   }).then(response=>response.json())
-  .then((datas)=>console.log(datas))
-  .catch(error => {
-    console.log('Error deleting product:', error);
+  return response.json();
+})
+.then((categories) => {
+  const categorySelect = document.getElementById('category-filter');
+  categories.forEach((category) => {
+    const option = document.createElement('option');
+    option.value = category.name;
+    option.textContent = category.name;
+    categorySelect.appendChild(option);
   });
-  }
-  console.log('delete');
+})
+.catch((error) => {
+  console.error(error);
+});
+
+  function fetchProduct() {
+      return new Promise((resolve, reject) => {
+             fetch("https://api.escuelajs.co/api/v1/products")
+                .then((res) => res.json())
+                .then((products) => {
+                        console.log(products);
+                        resolve(products);
+                    })
+                .catch((err) => {
+                        console.log(err);
+                    });
+            });
+        }
+
+  fetchProduct()
+      .then((products) => {
+                console.log(products);
+                data = products;
+                filteredProducts= products;
+                displayProducts(products);
+                renderPagination();
+            })
+      .catch((err) => {
+                console.error(err);
+            });
+
+  function displayProducts(products) {
+            
+            const container = document.getElementById("product-container");
+            container.innerHTML = "";
+
+            // Apply sorting
+            const sortBy = document.getElementById('sort-select').value;
+            if (sortBy === 'price-low-to-high') {
+                products.sort((a, b) => a.price - b.price);
+            } else if (sortBy === 'price-high-to-low') {
+                products.sort((a, b) => b.price - a.price);
+            }
+
+            // Apply search filter
+            const searchQuery = document.getElementById('search-input').value.trim().toLowerCase();
+            const filteredData = products.filter(product => {
+                const titleMatch = product.title.toLowerCase().includes(searchQuery);
+                const descriptionMatch = product.description.toLowerCase().includes(searchQuery);
+                return titleMatch || descriptionMatch;
+            });
+
+            const startIndex = (currentPage - 1) * itemsPerPage;
+            const endIndex = startIndex + itemsPerPage;
+            const displayedProducts = filteredData.slice(startIndex, endIndex);
+
+            displayedProducts.forEach((product) => {
+                const createDiv = document.createElement("div");
+                createDiv.classList.add("card");
+                createDiv.innerHTML = `
+                    <div>${product.title}</div>
+                    <div class="image-container">
+                        <img src="${product.images[0]}" alt="image" width="50" />
+                    </div>
+                    <div>Rs.${product.price}</div>
+                    <details><summary>Description</summary>
+                    ${product.description}<br>
+                    </details>
+                    <button class="btn btn-warning" onclick="editProduct(${product.id})">Edit</button>
+                    <button class="btn btn-danger" onclick="deleteProduct(${product.id})">Delete</button>
+                `;
+                container.appendChild(createDiv);
+            });
+        }
   
-}
-const searchInput = document.querySelector('#search-input');
-searchInput.addEventListener('input', () => {
-  const searchQuery = searchInput.value.trim().toLowerCase();
-  const filteredData = data.filter(product => {
-    const titleMatch = product.title.toLowerCase().includes(searchQuery);
-    const descriptionMatch = product.description.toLowerCase().includes(searchQuery);
-    return titleMatch || descriptionMatch;
-  });
-  displayProducts(filteredData);
-});
+  function editProduct(id){
+          console.log(id);
+          window.location.href = 'Update/update.html?id=' + id;;
+      }
 
+  function deleteProduct(productId) {
+            const userResponse = confirm('Do you want to delete');
+            if (userResponse) {
+                fetch(`https://api.escuelajs.co/api/v1/products/${productId}`, {
+                    method: "DELETE"
+                })
+                .then(response => response.json())
+                .then(() => {
+                    fetchProduct()
+                        .then((products) => {
+                            data = products;
+                            displayProducts(data);
+                            renderPagination();
+                        })
+                        .catch((err) => {
+                            console.error(err);
+                        });
+                })
+                .catch(error => {
+                    console.log('Error deleting product:', error);
+                });
+            }
+           
+        }
+
+  const sortSelect = document.getElementById('sort-select');
+        sortSelect.addEventListener('change', () => {
+            currentPage = 1;
+            // displayProducts(data);
+            displayProducts(filteredProducts);
+            renderPagination();
+        });
+
+  const searchInput = document.querySelector('#search-input');
+        searchInput.addEventListener('input', () => {          
+            currentPage = 1;
+            // displayProducts(data);
+            displayProducts(filteredProducts);
+            renderPagination();
+        });
+
+
+  document.getElementById('category-filter').addEventListener('change', function () {
+
+          console.log('filter');
+          let categorySelected = document.getElementById('category-filter').value;
+      
+      
+          if (categorySelected !== 'all') {
+              // console.log(products)
+              console.log(categorySelected)
+              
+              filteredProducts = data.filter((product) => {
+                  return product.category && product.category.name === categorySelected;
+              });
+              console.log(filteredProducts)
+          } else if(categorySelected === 'all') {
+            console.log("going")
+              filteredProducts = data;
+              // displayProducts(filteredProducts);
+              // renderPagination();
+          }
+          currentPage = 1;
+          console.log(filteredProducts)
+          
+          displayProducts(filteredProducts);
+          renderPagination();
+      });
+
+  function renderPagination() {
+            const paginationContainer = document.getElementById('pagination');
+            const totalPages = Math.ceil(filteredProducts.length / itemsPerPage);
+
+            const paginationHTML = Array.from({ length: totalPages }, (_, index) => {
+                const page = index + 1;
+                const activeClass = currentPage === page ? 'active' : '';
+                return `<button onclick="changePage(${page})" class="btn btn-primary ${activeClass}" >${page}</button> &nbsp`;
+            }).join('');
+
+            paginationContainer.innerHTML = paginationHTML;
+        }
+
+  function changePage(page) {
+            currentPage = page;
+            displayProducts(filteredProducts);
+            renderPagination();
+        }
